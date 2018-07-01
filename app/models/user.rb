059fe23e477ea23a4d5fdd74d2aac82fda1adbd1
vincent535 <taiuka3@icloud.com>
2018-07-01 16:20:25 +0900

    電話番号認証したコードです。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+   validates :name, presence: true
+  def email_required?
+    false
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
