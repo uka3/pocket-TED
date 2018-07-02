c01c8cf811e3f1b1a882f424df68e4c1a3ff8dfa
vincent535 <taiuka3@icloud.com>
2018-07-02 17:24:23 +0900

    まだ解決できてないです

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  beefore_filter :configurepermitted_parameters, if: :devise_controller?
+
+  protected
+  def configurepermitted_parameters
+    devise_parameter_santaizer.for(:sign_up){ |u| u.permit(:username, :phonenumber, :password, :password_confirmation :remember_me) }
+    devise_parameter_santaizer.for(sign_in){ |u| u.permit(:login, :username, :phonenumber, :password, :remember_me) }
+    devise_parameter_santaizer.for(:account_update) { |u| u.permit(:username, :phonenumber, :password, :password_cpnfirmation, :surrent_password) }
+  end
+
+
+
+
+
+
 end
